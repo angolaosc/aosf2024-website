3def3c3a23554b286d02dbc1538739a9e120ca43
Jessé João Carlos Inglês <JesseIngles@hotmail.com>
2024-08-28 04:58:26 +0100

    removed other-events section from homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -18,7 +18,7 @@ export default function HomePage() {
         <CompaniesSection />
         <EventDetails />
         <EventTopics />
-        <OtherEvents />
+        
         <PartnersSection />
       </main>
       <Footer />
