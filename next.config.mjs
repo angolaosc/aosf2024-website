2b5fd1e5dae6a5e040560777af1bf4fc165dd967
Jessé João Carlos Inglês <JesseIngles@hotmail.com>
2024-11-20 16:28:45 +0100

    adicionado speakers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cache.sessionize.com',
+        port: '', 
+        pathname: '/**', 
+      },
+    ],
+  },
+};
 
 export default nextConfig;
